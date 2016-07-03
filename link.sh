032ebdfd346d5ad790825a26a34dc17e5e7a4fd3
root <travis.gillin@gmail.com>
2016-07-03 18:01:48 -0400

    Added openbox config

--- link.sh
+++ link.sh
@@ -5,8 +5,10 @@ rm /bin/git-completion.bash
 rm ~/.bashrc
 rm ~/.nanorc
 rm -r ~/.nano
+rm ~/.config/openbox/rc.xml
 
 ln -s $PWD/git-completion.bash /bin/git-completion.bash
 ln -s $PWD/my.bashrc ~/.bashrc
 ln -s $PWD/my.nanorc ~/.nanorc
 ln -s $PWD/my.nano ~/.nano
+ln -s $PWD/my.openbox ~/.config/openbox/rc.xml
